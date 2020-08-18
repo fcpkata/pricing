d69c3d49572478b11666efb04a8daeb14f411525
Tamilselvi <PTAMILS3@ford.com>
2020-08-18 21:31:13 +0530

    refactored testcase

--- src/test/java/com/kata/priceservice/service/WeightBasedShippingPriceCalculatorTest.java
+++ src/test/java/com/kata/priceservice/service/WeightBasedShippingPriceCalculatorTest.java
@@ -1,15 +1,20 @@
 package com.kata.priceservice.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
 import com.kata.priceservice.model.ShippingPriceRequest;
-@RunWith(MockitoJUnitRunner.class)
+
+@RunWith(Parameterized.class)
 public class WeightBasedShippingPriceCalculatorTest {
 
 	private WeightBasedShippingPriceCalculator calculator;
@@ -17,100 +22,38 @@ public class WeightBasedShippingPriceCalculatorTest {
 	@Mock
 	private ShippingPriceCalculator mockShippingPriceCalculator;
 
+	private ShippingPriceRequest shippingPriceRequest;
+
+	private int expectedResult;
+
 	@Before
-	public void setUp() {
+	public void initialize() {
 		calculator = new WeightBasedShippingPriceCalculator();
 	}
 
-	@Test
-	public void shouldReturn0WhenWeightIsGivenAs0() {
-
-		ShippingPriceRequest request = ShippingPriceRequest.builder().weight(0).build();
-
-		assertThat(calculator.getPrice(request)).isEqualTo(0);
+	@Parameterized.Parameters
+	public static Collection itemWeights() {
+		return Arrays.asList(new Object[][] { { ShippingPriceRequest.builder().weight(0).build(), 0 },
+				{ ShippingPriceRequest.builder().weight(100).build(), 0 },
+				{ ShippingPriceRequest.builder().weight(250).build(), 10 },
+				{ ShippingPriceRequest.builder().weight(251).build(), 25 },
+				{ ShippingPriceRequest.builder().weight(500).build(), 25 },
+				{ ShippingPriceRequest.builder().weight(501).build(), 50 },
+				{ ShippingPriceRequest.builder().weight(2000).build(), 50 },
+				{ ShippingPriceRequest.builder().weight(2001).build(), 75 },
+				{ ShippingPriceRequest.builder().weight(10000).build(), 75 },
+				{ ShippingPriceRequest.builder().weight(10001).build(), 150 },
+				{ ShippingPriceRequest.builder().weight(10001).build(), 150 } });
 	}
-	
-	@Test
-	public void shouldReturn0WhenWeightGivenAs100() {
 
-		ShippingPriceRequest request = ShippingPriceRequest.builder().weight(100).build();
-
-		assertThat(calculator.getPrice(request)).isEqualTo(0);
+	public WeightBasedShippingPriceCalculatorTest(ShippingPriceRequest request, int expectedResult) {
+		this.shippingPriceRequest = request;
+		this.expectedResult = expectedResult;
 	}
-	
+
 	@Test
-	public void shouldReturn10WhenWeightGivenAs250() {
-
-		ShippingPriceRequest request = ShippingPriceRequest.builder().weight(250).build();
-
-		assertThat(calculator.getPrice(request)).isEqualTo(10);
+	public void shouldReturnExpectedPriceForGivenWeight() {
+		assertEquals(calculator.getPrice(shippingPriceRequest), expectedResult);
 	}
-	
-	@Test
-	public void shouldReturn25WhenWeightGivenAs251() {
-
-		ShippingPriceRequest request = ShippingPriceRequest.builder().weight(251).build();
-
-		assertThat(calculator.getPrice(request)).isEqualTo(25);
-	}
-	
-	@Test
-	public void shouldReturn25WhenWeightGivenAs500() {
-
-		ShippingPriceRequest request = ShippingPriceRequest.builder().weight(500).build();
-
-		assertThat(calculator.getPrice(request)).isEqualTo(25);
-	}
-	
-	@Test
-	public void shouldReturn50WhenWeightGivenAs501() {
-
-		ShippingPriceRequest request = ShippingPriceRequest.builder().weight(501).build();
-
-		assertThat(calculator.getPrice(request)).isEqualTo(50);
-	}
-	
-	
-	@Test
-	public void shouldReturn50WhenWeightGivenAs2000() {
-
-		ShippingPriceRequest request = ShippingPriceRequest.builder().weight(2000).build();
-
-		assertThat(calculator.getPrice(request)).isEqualTo(50);
-	}
-	
-	@Test
-	public void shouldReturn50WhenWeightGivenAs2001() {
-
-		ShippingPriceRequest request = ShippingPriceRequest.builder().weight(2001).build();
-
-		assertThat(calculator.getPrice(request)).isEqualTo(75);
-	}
-	
-	@Test
-	public void shouldReturn50WhenWeightGivenAs10000() {
-
-		ShippingPriceRequest request = ShippingPriceRequest.builder().weight(10000).build();
-
-		assertThat(calculator.getPrice(request)).isEqualTo(75);
-	}
-	
-	@Test
-	public void shouldReturn50WhenWeightGivenAs10001() {
-
-		ShippingPriceRequest request = ShippingPriceRequest.builder().weight(10001).build();
-
-		assertThat(calculator.getPrice(request)).isEqualTo(150);
-	}
-	
-	@Test
-	public void shouldReturn50WhenWeightGivenAs10001AndNextCalculatorIsNull() {
-
-		ShippingPriceRequest request = ShippingPriceRequest.builder().weight(10001).build();
-		calculator.setNextCalculator(null);
-
-		assertThat(calculator.getPrice(request)).isEqualTo(150);
-	}
-	
 
 }
